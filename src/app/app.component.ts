1f5541d098590a7fefbc1158dbb293bd163574ec
miguel81234 <ortizrendonmiguelangel@gmail.com>
2026-03-04 06:38:00 -0500

    consumir-api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PersonajeService, Personaje } from './services/personaje.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']   // ← ESTA LÍNEA FALTABA
 })
-export class AppComponent {
-  title = 'rick-app';
+export class AppComponent implements OnInit {
+
+  personaje!: Personaje;
+
+  constructor(private personajeService: PersonajeService) {}
+
+  ngOnInit(): void {
+    this.personajeService.getPersonaje().subscribe({
+      next: (data) => {
+        this.personaje = data;
+      },
+      error: (err) => {
+        console.error('Error al obtener personaje', err);
+      }
+    });
+  }
 }
---
+++ src/app/services/personaje.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Personaje {
+  id: number;
+  name: string;
+  status: string;
+  species: string;
+  type: string;
+  gender: string;
+  origin: {
+    name: string;
+    url: string;
+  };
+  location: {
+    name: string;
+    url: string;
+  };
+  image: string;
+  episode: string[];
+  url: string;
+  created: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PersonajeService {
+
+  private apiUrl = 'https://rickandmortyapi.com/api/character/441';
+
+  constructor(private http: HttpClient) {}
+
+  getPersonaje(): Observable<Personaje> {
+    return this.http.get<Personaje>(this.apiUrl);
+  }
+}
